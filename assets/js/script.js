297047e84a8263eb78d37c25336ca003194ac8ee
ziyad1412 <abdurrahman.ziyad17@gmail.com>
2022-09-25 21:13:04 +0700

    Tugas 7 Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,146 @@
+$(document).ready(function(){
+    //sapi
+    $(".cow").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/cow.png",
+            'class':"img-fluid",
+        }).appendTo(".cow");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/cow.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".cow")
+    })
+    //domba
+    $(".sheep").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/sheep.png",
+            'class':"img-fluid",
+        }).appendTo(".sheep");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/sheep.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".sheep")
+    })
+    //kerbau
+    $(".buffalo").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/buffalo.png",
+            'class':"img-fluid",
+        }).appendTo(".buffalo");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/buffalo.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".buffalo")
+    })
+    //alpaka
+    $(".alpaca").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/alpaca.png",
+            'class':"img-fluid",
+        }).appendTo(".alpaca");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/alpaca.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".alpaca")
+    })
+    //alpaka
+    $(".chicken").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/chicken.png",
+            'class':"img-fluid",
+        }).appendTo(".chicken");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/chicken.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".chicken")
+    })
+    //merpati
+    $(".dove").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/dove.png",
+            'class':"img-fluid",
+        }).appendTo(".dove");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/dove.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".dove")
+    })
+    //bebek
+    $(".duck").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/duck.png",
+            'class':"img-fluid",
+        }).appendTo(".duck");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/duck.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".duck")
+    })
+    //bebek
+    $(".swan").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/swan.png",
+            'class':"img-fluid",
+        }).appendTo(".swan");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/swan.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".swan")
+    })
+    //singa
+    $(".lion").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/lion.png",
+            'class':"img-fluid",
+        }).appendTo(".lion");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/lion.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".lion")
+    })
+    //harimau
+    $(".tiger").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/tiger.png",
+            'class':"img-fluid",
+        }).appendTo(".tiger");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/tiger.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".tiger")
+    })
+    //hiu
+    $(".wolf").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/wolf.png",
+            'class':"img-fluid",
+        }).appendTo(".wolf");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/wolf.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".wolf")
+    })
+    //komodo
+    $(".komodo-dragon").click(function(){
+        $("<img>").attr({
+            'src':"assets/img/komodo-dragon.png",
+            'class':"img-fluid",
+        }).appendTo(".komodo-dragon");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/komodo-dragon.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".komodo-dragon")
+    })
+})
