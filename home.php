ef5e9342a4db4c1c12acaeadbb6409100b416f9e
ziyad1412 <abdurrahman.ziyad17@gmail.com>
2022-12-15 09:16:27 +0700

    Tugas 6 PHP

---
+++ home.php
@@ -0,0 +1,16 @@
+<header id="header" class="header">
+  <div class="container">
+    <h1><a href="index.php">Abdurrahman Ziyad</a></h1>
+    <!-- Uncomment below if you prefer to use an image logo -->
+    <!-- <a href="index.html" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
+    <h2>I'm a college student<span> Fulltack Web Developer</span></h2>
+
+    <?php include_once 'navbar.php' ?>
+
+    <!--- ini sosmed.php --->
+    <div class="social-links">
+      <a href="https://www.instagram.com/kajicode" target="_blank" class="instagram" target="_blank" class="instagram"><i class="bi bi-instagram"></i></a>
+      <a href="https://www.linkedin.com/in/abdurrahmanziyad/" target="_blank" class="linkedin"><i class="bi bi-linkedin"></i></a>
+    </div>
+  </div>
+</header>
